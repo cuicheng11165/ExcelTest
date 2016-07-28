09455e60152e2bec2543a2baba8fe89a19926e41
Cheng Cui <cuicheng11165@gmail.com>
2016-07-28 11:12:06 +0800

    add

---
+++ ExcelUtl/Program.cs
@@ -0,0 +1,184 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using ExcelUtl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcelTest
+{
+    public class TestEntity
+    {
+        [SheetColumn("")]
+        public string TestString { set; get; }
+
+        [SheetColumn("")]
+        public int TestInt32 { set; get; }
+
+        [SheetColumn("")]
+        public double TestDouble { set; get; }
+
+        [SheetColumn("")]
+        public bool TestBoolean { set; get; }
+
+        [SheetColumn("")]
+        public DateTime TestDateTime { set; get; }
+    }
+
+    public class RowAtt : Attribute
+    {
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var entity = new TestEntity { TestString = "aqadsdasd", TestInt32 = 100, TestDouble = 1.2122, TestBoolean = true, TestDateTime = DateTime.Now };
+            var entity = new TestEntity { TestString = "aqadsdasd", TestInt32 = 100, TestDouble = 1.2122, TestBoolean = true, TestDateTime = DateTime.Now };
+            var list = new List<TestEntity>() { entity };
+
+            var document = new SpreadsheetImport("d:\\test2.xlsx", "sheet1");
+
+            document.AddElements(list);
+
+            document.Dispose();
+
+            return;
+
+
+
+            CreateExcel("d:\\test.xlsx", "name1", new string[][] { new string[] { "Column1", "Column2" }, new string[] { "222", "333" }, new string[] { "111", "2222" } });
+
+            //return;
+
+            SpreadsheetDocument spreadsheetDocument =
+                    SpreadsheetDocument.Create("d:\\test1.xlsx", SpreadsheetDocumentType.Workbook);
+
+            // Add a WorkbookPart and Workbook objects.
+            WorkbookPart workbookpart = spreadsheetDocument.AddWorkbookPart();
+            workbookpart.Workbook = new Workbook();
+
+            // Add a WorksheetPart.
+            WorksheetPart worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
+
+            // Create Worksheet and SheetData objects.
+            worksheetPart.Worksheet = new Worksheet(new SheetData());
+
+            // Add a Sheets object.
+            Sheets sheets = spreadsheetDocument.WorkbookPart.Workbook
+                .AppendChild<Sheets>(new Sheets());
+
+            // Append the new worksheet named "mySheet" and associate it 
+            // with the workbook.
+            Sheet sheet = new Sheet()
+            {
+                Id = spreadsheetDocument.WorkbookPart
+                    .GetIdOfPart(worksheetPart),
+                SheetId = 1,
+                Name = "mySheet"
+            };
+            sheets.Append(sheet);
+
+            // Get the sheetData cell table.
+            SheetData sheetData = worksheetPart.Worksheet.GetFirstChild<SheetData>();
+
+            // Add a row to the cell table.
+            Row row;
+            row = new Row() { RowIndex = 1 };
+            sheetData.Append(row);
+
+            // Add the cell to the cell table at A1.
+            Cell refCell = null;
+            Cell newCell = new Cell() { CellReference = "A1" };
+            row.InsertBefore(newCell, refCell);
+
+            // Set the cell value to be a numeric value of 123.
+            newCell.CellValue = new CellValue("123");
+            newCell.DataType = new EnumValue<CellValues>(CellValues.Number);
+
+            // Close the document.
+            spreadsheetDocument.Close();
+
+            Console.WriteLine("All done. Press a key");
+            Console.ReadKey();
+
+        }
+
+        public static void CreateExcel(string path, string sheetName, string[][] data)
+        {
+            using (SpreadsheetDocument spreadsheet = SpreadsheetDocument.Create(path, SpreadsheetDocumentType.Workbook))
+            {
+                WorkbookPart workbookpart = spreadsheet.AddWorkbookPart();
+                workbookpart.Workbook = new Workbook();
+                WorksheetPart worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
+                worksheetPart.Worksheet = new Worksheet(new SheetData());
+
+                SharedStringTablePart shareStringPart;
+
+                if (workbookpart.GetPartsOfType<SharedStringTablePart>().Count() > 0)
+                {
+                    shareStringPart = workbookpart.GetPartsOfType<SharedStringTablePart>().First();
+                }
+                else
+                {
+                    shareStringPart = workbookpart.AddNewPart<SharedStringTablePart>();
+                }
+                shareStringPart.SharedStringTable = new SharedStringTable();
+                shareStringPart.SharedStringTable.AppendChild(new SharedStringItem(new Text("50")));
+                shareStringPart.SharedStringTable.Save();
+
+
+                Sheets sheets = spreadsheet.WorkbookPart.Workbook.AppendChild<Sheets>(new Sheets());
+                if (sheetName != null && data.Count() > 0)
+                {
+                    Sheet sheet = new Sheet()
+                    {
+                        Id = spreadsheet.WorkbookPart.GetIdOfPart(worksheetPart),
+                        SheetId = 1,
+                        Name = sheetName
+                    };
+                    SheetData sheetData = worksheetPart.Worksheet.GetFirstChild<SheetData>();
+
+                    for (int i = 0; i < data.Count(); i++)
+                    {
+                        sheetData.Append(CreateContentRow(data[i], i + 1));
+                    }
+                    sheets.Append(sheet);
+                }
+                workbookpart.Workbook.Save();
+                worksheetPart.Worksheet.Save();
+                spreadsheet.Close();
+            }
+        }
+
+        private static Row CreateContentRow(string[] cells, int rowIndex)
+        {
+            Row row = new Row
+            {
+                RowIndex = (UInt32)rowIndex
+            };
+            for (int i = 0; i < cells.Length; i++)
+            {
+                Cell dataCell = createTextCell(i + 1, rowIndex, cells[i]);
+                row.AppendChild(dataCell);
+            }
+            return row;
+        }
+
+        private static Cell createTextCell(int columnIndex, int rowIndex, object cellValue)
+        {
+            Cell cell = new Cell();
+            cell.DataType = CellValues.InlineString;
+            InlineString inlineString = new InlineString();
+            Text t = new Text();
+            t.Text = cellValue == null ? string.Empty : cellValue.ToString();
+            cell.CellValue = new CellValue(t.Text);
+            inlineString.AppendChild(t);
+            cell.AppendChild(inlineString);
+            return cell;
+        }
+    }
+}
+
---
+++ ExcelUtl/SpreadsheetImport.cs
@@ -0,0 +1,195 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ExcelUtl
+{
+    public class SpreadsheetImport : IDisposable
+    {
+        SpreadsheetDocument spreadsheet;
+        Sheets sheets;
+        WorkbookPart workbookpart;
+        WorksheetPart worksheetPart;
+        Sheet sheet;
+        uint rowIndex = 1;
+        bool columnHeaderGerated = false;
+        private Dictionary<PropertyInfo, string> properties;
+
+        public SpreadsheetImport(string outPutFilePath, string sheetName)
+        {
+            // Create a spreadsheet document by supplying the file name.
+            spreadsheet = SpreadsheetDocument.Create(outPutFilePath, SpreadsheetDocumentType.Workbook);
+
+            // Add a WorkbookPart to the document.
+            workbookpart = spreadsheet.AddWorkbookPart();
+            workbookpart.Workbook = new Workbook();
+
+            // Add a WorksheetPart to the WorkbookPart.
+            worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
+            worksheetPart.Worksheet = new Worksheet(new SheetData());
+
+            // Add Sheets to the Workbook.
+            sheets = spreadsheet.WorkbookPart.Workbook.AppendChild<Sheets>(new Sheets());
+
+            sheet = new Sheet()
+            {
+                Id = spreadsheet.WorkbookPart.GetIdOfPart(worksheetPart),
+                SheetId = 1,
+                Name = sheetName
+            };
+
+        }
+
+        public void AddElements<T>(IList<T> elements)
+        {
+            SheetData sheetData = worksheetPart.Worksheet.GetFirstChild<SheetData>();
+
+            if (properties == null)
+            {
+                properties = GetProperties(typeof(T));
+                sheetData.Append(CreateColumnHeader(properties));
+            }
+
+            sheetData.Append(elements.Select(CreateContentRow));
+
+        }
+
+        private Row CreateContentRow<T>(T element)
+        {
+            var row = new Row() { RowIndex = rowIndex++ };
+            int cellIndex = 0;
+            foreach (var entry in properties)
+            {
+                var value = entry.Key.GetValue(element, null);
+                row.AppendChild(CreateCell(value, cellIndex++, entry.Value));
+            }
+            return row;
+        }
+
+
+
+        private Row CreateColumnHeader(Dictionary<PropertyInfo, string> properties)
+        {
+            var row = new Row() { RowIndex = rowIndex++ };
+            int cellIndex = 0;
+            if (!columnHeaderGerated)
+            {
+                foreach (var entry in properties)
+                {
+                    row.AppendChild(CreateCell(entry.Value, cellIndex++, entry.Value));
+                }
+                columnHeaderGerated = true;
+            }
+            return row;
+        }
+
+        private static Dictionary<PropertyInfo, string> GetProperties(Type type)
+        {
+            Dictionary<PropertyInfo, string> properties = new Dictionary<PropertyInfo, string>();
+
+            foreach (var propertyInfo in type.GetProperties())
+            {
+                var attributes = propertyInfo.GetCustomAttributes(typeof(SheetColumn), true);
+
+                string columnHeader = string.Empty;
+                bool hasSheetColumnAttribute = false;
+
+                foreach (var attr in attributes)
+                {
+                    var sheetColumnAttr = attr as SheetColumn;
+
+                    if (sheetColumnAttr != null)
+                    {
+                        columnHeader = sheetColumnAttr.ColumnName;
+                        hasSheetColumnAttribute = true;
+                    }
+                }
+
+                if (!hasSheetColumnAttribute)
+                {
+                    continue;
+                }
+
+                if (columnHeader == string.Empty)
+                {
+                    columnHeader = propertyInfo.Name;
+                }
+                properties[propertyInfo] = columnHeader;
+            }
+
+            return properties;
+        }
+
+        private Cell CreateCell(object cellValue, int cellIndex, string headerName)
+        {
+            if (cellValue is Int32 || cellValue is double)
+            {
+                return new NumberCell(headerName, cellValue.ToString(), cellIndex);
+            }
+            else if (cellValue is bool)
+            {
+                return new BooleanCell(headerName, (bool)cellValue, cellIndex);
+            }
+            return new TextCell(headerName, cellValue == null ? string.Empty : cellValue.ToString(), cellIndex);
+        }
+
+        public void Dispose()
+        {
+            sheets.Append(sheet);
+            workbookpart.Workbook.Save();
+            worksheetPart.Worksheet.Save();
+            spreadsheet.Close();
+        }
+    }
+
+    public class SheetColumn : Attribute
+    {
+        public SheetColumn(string columnName) { this.ColumnName = columnName; }
+        public string ColumnName { get; set; }
+    }
+
+
+    public class BooleanCell : Cell
+    {
+        public BooleanCell(string header, bool value, int index)
+        {
+            this.DataType = CellValues.Boolean;
+            this.CellReference = header + index;
+            //Add text to the text cell.
+            this.CellValue = new CellValue(BooleanValue.FromBoolean(value));
+        }
+    }
+    public class TextCell : Cell
+    {
+        public TextCell(string header, string text, int index)
+        {
+            this.DataType = CellValues.InlineString;
+            this.CellReference = header + index;
+            //Add text to the text cell.
+            this.InlineString = new InlineString { Text = new Text { Text = text } };
+        }
+    }
+    public class NumberCell : Cell
+    {
+        public NumberCell(string header, string text, int index)
+        {
+            this.DataType = CellValues.Number;
+            this.CellReference = header + index;
+            this.CellValue = new CellValue(text);
+        }
+    }
+
+    public class HeaderCell : TextCell
+    {
+        public HeaderCell(string header, string text, int index) :
+            base(header, text, index)
+        {
+            this.StyleIndex = 11;
+        }
+    }
+}
